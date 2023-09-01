d946176df22f5743440cdd19a56cf9138fd2e3a2
bharadiya <shashank.bharadiya@gmail.com>
2023-09-01 13:34:25 +0530

    new branch and new code pushed

--- src/Test/testonSD.java
+++ src/Test/testonSD.java
@@ -8,37 +8,31 @@ import org.testng.annotations.Test;
 import PageObjects.Login;
 import Utility.ReadConfigFile;
 
-public class testonSD
-{
-	public static WebDriver driver;
-	static ReadConfigFile rc = new ReadConfigFile("C:\\Users\\Sourabh Patni\\eclipse-workspace\\SauceDemoProject\\src\\HardcodeDependencies\\config.properties");
+public class testonSD {
+	public static WebDriver driver = new EdgeDriver();;
+	static ReadConfigFile rc = new ReadConfigFile(
+			"./src\\HardcodeDependencies\\config.properties");
 	static Login L = new Login(driver);
-	
-	
+
 	@BeforeTest
-	public static void openandlaunchBrowserURL()
-	{
-		driver = new EdgeDriver();
-		driver.get(rc.getApplicationURL());	
+	public static void openandlaunchBrowserURL() {
+		driver.get(rc.getApplicationURL());
 		driver.manage().window().maximize();
 	}
-		
+
 	@Test(priority = 1)
-	public static void loginToSDemo() 
-	{
+	public static void loginToSDemo() {
 		Login L = new Login(driver);
 		L.enteruserName(rc.getUserName());
 		L.enterPassword(rc.getPassword());
-		L.clickLoginButton();		
+		L.clickLoginButton();
 	}
-	
+
 	@Test(priority = 2)
-	public static void getgreatestPrice() 
-	{
-		//L.selectgreatestprice();
-		L.clickonAddtoCart();;
+	public static void getgreatestPrice() {
+		// L.selectgreatestprice();
+		L.clickonAddtoCart();
+		;
 	}
-	
-	
 
 }
